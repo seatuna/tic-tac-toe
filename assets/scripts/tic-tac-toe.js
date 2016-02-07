e6895732bfb53b43f09e814e8f6949cc70cddf2f
Celena T <chibiynm@gmail.com>
2016-02-07 02:43:46 -0500

    Change image url 2

--- assets/scripts/tic-tac-toe.js
+++ assets/scripts/tic-tac-toe.js
@@ -162,8 +162,8 @@ $('#sign-out').on('click', function(e) {
 // Start Game Logic
 
 let winner;
-let x = '<img src="http://img.photobucket.com/albums/v53/chibi_ynm/red_zpsmqrv6gkb.png">'; // Red mushroom
-let o = '<img src="http://img.photobucket.com/albums/v53/chibi_ynm/green_zps8csozywv.png">'; // Green 1up mushroom
+let x = '<img src="assets/images/red.png">'; // Red mushroom
+let o = '<img src="assets/images/green.png">'; // Green 1up mushroom
 let player = x; // Game starts with red mushroom going first
 let xWins = 0;
 let oWins = 0;
